2c6b7c8f3bfce140c49cb7526a49f0877143e5dc
P22inolan <p22inolan@fairfieldprep.org>
2021-09-28 10:04:03 -0400

    Submit 9/28

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author: IBN (AMDG) 9/28/2021
+
+number = input("Give me a number. ")
+
+if (int(number) % 2) == 0:
+    print("This is an even number.")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: IBN (AMDG) 9/28/2021
+
+number = input("Give me a number. ")
+
+if (int(number) % 2) == 0:
+    print("The number " + str(number) + " is even.")
+else:
+    print("The number " + str(number) + " is odd.")
