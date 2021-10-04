b5223ab9f891db5136730a85d5f1062ab1ab95a8
P22inolan <p22inolan@fairfieldprep.org>
2021-10-04 09:57:24 -0400

    Lab 5 10/4

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: IBN (AMDG) 10/4/2021
+
+# int(a)
+# NameError
+
+# int('a')
+# ValueError
+
+# 'a'+2
+# TypeError
+
+# import date
+# ModuleNotFoundError
+
+# print("I'm a happy camper!)
+# SyntaxError
